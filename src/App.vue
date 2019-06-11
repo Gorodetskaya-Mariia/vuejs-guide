159f865f0d2df7715dc7d7412c1e12316638305f
Mariia <1907mss@gmail.com>
2019-06-11 23:16:30 +0300

    installing the vue-cli

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hello {{ status }}</h1>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      status: 'kvnfv'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
