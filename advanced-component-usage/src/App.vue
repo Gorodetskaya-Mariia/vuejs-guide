596e39030e0c21fbe7fc5c62ac69122df4073af3
Mariia <1907mss@gmail.com>
2019-06-12 16:39:03 +0300

    usage slots

---
+++ advanced-component-usage/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+      <app-quote>
+        <h2 slot="title">{{ quoteTitle }}</h2>
+        <p slot="content">A wonderful Quote!</p>
+      </app-quote>
+    </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+
+  export default {
+    data: function() {
+      return {
+        quoteTitle: 'The Quotes'
+      }
+    },
+    components: {
+      appQuote: Quote
+    }
+  };
+
+</script>
+
+<style>
+
+</style>
+
