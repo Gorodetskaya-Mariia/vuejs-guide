596504e31169e8bfcac19f49878dcf0ff0d0dbe7
Mariia <1907mss@gmail.com>
2019-06-12 23:41:37 +0300

    quote application is started

---
+++ quote application/src/components/NewQuote.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+      <form>
+        <div clas="">
+          <label>Quote</label>
+          <textarea class="form-control" rows="3" v-model="quote"></textarea>
+        </div>
+        <div>
+          <button class="btn" @click.prevent="createNew">Add Quote</button>
+        </div>
+      </form>
+    </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        quote:''
+      }
+    },
+    methods: {
+      createNew() {
+
+      }
+    }
+  };
+
+</script>
+
+<style>
+
+</style>
