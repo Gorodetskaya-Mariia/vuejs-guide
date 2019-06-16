b5162f019a422a4829b9ef6582abc9dc6088d7d0
Mariia <1907mss@gmail.com>
2019-06-16 15:15:21 +0300

    controls On Off are added

--- form-of-a-complaint/src/App.vue
+++ form-of-a-complaint/src/App.vue
@@ -11,7 +11,9 @@
                                 type="text"
                                 id="email"
                                 class="form-control"
-                                v-model="userData.email">
+                                :value="userData.email"
+																@input="userData.email = $event.target.value">
+																<!-- alternative:  v-model="userData.email" -->
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
@@ -93,6 +95,11 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+              <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <app-switch v-model="dataSwitch"></app-switch>
+              </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -120,7 +127,7 @@
                         </ul>
                         <p>Gender: {{ gender }}</p>
                         <p>Priority: {{ selectedPriority}}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{ dataSwitch}}</p>
                     </div>
                 </div>
             </div>
@@ -129,6 +136,7 @@
 </template>
 
 <script>
+    import Switch from "./Switch.vue";
     export default {
 			data () {
         return {
@@ -141,8 +149,12 @@
           sendMails: [],
           gender: 'Male',
           priorities: ['High', 'Medium', 'Low'],
-          selectedPriority: 'High'
+          selectedPriority: 'High',
+          dataSwitch: true
         }
+      },
+      components: {
+        appSwitch: Switch
       }
     }
 </script>
