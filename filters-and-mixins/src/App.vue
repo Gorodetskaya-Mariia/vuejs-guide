c32683cef71900b56f2900d426e5376c83175b26
Mariia <1907mss@gmail.com>
2019-06-17 23:48:55 +0300

    mixin and scope

--- filters-and-mixins/src/App.vue
+++ filters-and-mixins/src/App.vue
@@ -4,6 +4,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
                 <p>{{ text }}</p>
+                <button @click="fruits.push('Berries')">Add new Item</button>
                 <input v-model="filterText">
                 <ul>
                   <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
