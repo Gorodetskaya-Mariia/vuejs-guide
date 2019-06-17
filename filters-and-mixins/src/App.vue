d3545ce314ffd186196ebf6a3f3a63755a18f754
Mariia <1907mss@gmail.com>
2019-06-17 23:13:40 +0300

    a mixin is added

--- filters-and-mixins/src/App.vue
+++ filters-and-mixins/src/App.vue
@@ -3,41 +3,30 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-                <p> {{ text | toUppercase | to-lowcase}} </p>
-                <hr>
+                <p>{{ text }}</p>
                 <input v-model="filterText">
                 <ul>
                   <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
                 </ul>
             </div>
+            <app-list></app-list>
         </div>
     </div>
 </template>
 
 <script>
+    import List from "./List.vue";
+    import {fruitMixin} from "./fruitMixin"
     export default {
       data() {
         return {
           text: 'Hello there!',
-          fruits: ['Banana', 'Apple', 'Mango', 'Melon'],
-          filterText: ''
         }
       },
-      filters: {
-        // alternative:
-        // 'to-uppercase'(){}
-        // every filter must contain data->value
-        toUppercase(value) {
-          return value.toUpperCase();
-        }
+      components: {
+        appList: List
       },
-      computed: {
-        filteredFruits() {
-          return this.fruits.filter((element) =>{
-            return element.match(this.filterText);
-          });
-        }
-      }
+      mixins: [fruitMixin]
     }
 </script>
 
