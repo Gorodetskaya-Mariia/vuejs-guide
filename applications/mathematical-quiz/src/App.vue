3b25a63216c396f156bf2579eb6e035abbd34d2a
Mariia <1907mss@gmail.com>
2019-06-23 16:45:33 +0300

    creating maths app without animation

---
+++ applications/mathematical-quiz/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1 class="text-center">The Mathematical Quiz</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <component
+          :is="mode"
+          @answered="answered($event)"
+          @confirmed="mode = 'app-question'"
+        ></component>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Answer from "./components/Answer.vue";
+  import Question from "./components/Question.vue";
+
+  export default {
+    name: "app",
+    data() {
+      return {
+        mode: "app-question"
+      };
+    },
+    methods: {
+      answered(isCorrect) {
+        if (isCorrect) {
+          this.mode = "app-answer";
+        } else {
+          this.mode = "app-question";
+          alert("Wrong, try again!");
+        }
+      }
+    },
+    components: {
+      appAnswer: Answer,
+      appQuestion: Question
+    }
+  };
+</script>
+
+<style>
+</style>
