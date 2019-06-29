97ad9ca9ca296de7c79ad3193966f5bfeeff4996
Mariia <1907mss@gmail.com>
2019-06-29 18:30:20 +0300

    using route parameters

--- routing-in-vuejs-app/src/components/user/User.vue
+++ routing-in-vuejs-app/src/components/user/User.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     <h1>The User Page</h1>
+    <p>Loaded id: {{ id }}</p>
     <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
   </div>
 </template>
 
 <script>
   export default {
+    data() {
+        return {
+          id : this.$route.params.id
+        }
+    },
+    watch: {
+      '$route'(to, from) {
+        this.id = to.params.id;
+      }
+    },
     methods: {
       navigateToHome() {
         this.$router.push('/');
