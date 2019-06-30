eae906b25d06693a9f887ea9caacbbfd7970b677
Mariia <1907mss@gmail.com>
2019-06-30 15:05:23 +0300

    creating links with named routes

--- routing-in-vuejs-app/src/components/user/UserDetail.vue
+++ routing-in-vuejs-app/src/components/user/UserDetail.vue
@@ -1,3 +1,13 @@
 <template>
+  <div>
     <h3>Some User Details</h3>
+    <p>Loaded id: {{ $route.params.id }}</p>
+    <router-link
+      tag="button"
+      class="btn btn-primary"
+      :to="{ name: 'userEdit', params: { id: $route.params.id } }"
+    >Edit User
+    </router-link>
+  </div>
 </template>
+<!-- alternative for :to- "'/user/' + $route.params.id + '/edit'" -->
