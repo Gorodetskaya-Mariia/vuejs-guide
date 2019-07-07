3ba10b7b2b8bd55fd277690828c5591af0e39a8e
Mariia <1907mss@gmail.com>
2019-07-07 16:57:36 +0300

    a summaru of Vuex

--- state-management-with-vuex/src/store/store.js
+++ state-management-with-vuex/src/store/store.js
@@ -5,22 +5,58 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    counter: 0
+    counter: 0,  /*set up an initial state */
+    value: 0
   },
+  /* allow us to access a state in different components: Result.vue, AnotherResult.vue */
   getters: {
     doubleCounter: state=> {
       return state.counter * 2;
     },
     stringCounter: state=> {
       return state.counter;
+    },
+    value: state => {
+      return state.value;
     }
   },
+  /* to change our state we use mutations, mutations take state as import, payload is a data we pass to mutate the state*/
   mutations: {
-    increment: state=> {
-      state.counter++;
+    increment: (state, payload) => {
+      state.counter += payload;
     },
-    decrement: state=> {
-      state.counter--;
+    decrement: (state, payload) => {
+      state.counter -= payload;
+    },
+    updateValue: (state, payload) => {
+      state.value = payload;
+    }
+  },
+  /* to commit the mutations: commit({name of mutation}, passed value)*/
+  actions: {
+    // es6
+    increment:({ commit }, payload) => {
+      commit('increment', payload);
+    },
+    decrement:({ commit }, payload) => {
+      commit('decrement', payload);
+    },
+    // es5
+    // increment: context => {
+    //   context.commit('increment');
+    // }
+    asyncIncrement: ({ commit }, payload) => {
+      setTimeout(() => {
+        commit('increment', payload.by);
+      }, payload.duration);
+    },
+    asyncDecrement: ({ commit }, payload) => {
+      setTimeout(() => {
+        commit('decrement', payload.by);
+      }, payload.duration);
+    },
+    updateValue({commit}, payload) {
+      commit('updateValue', payload);
     }
   }
 });
