e5b16f099eee1323a02f12098368a4be11145de8
Mariia <1907mss@gmail.com>
2019-07-17 17:35:35 +0300

    using interceptors

--- using-axios/src/main.js
+++ using-axios/src/main.js
@@ -9,6 +9,18 @@ axios.defaults.baseURL = 'https://using-axios-e30c4.firebaseio.com/';
 axios.defaults.headers.common['Authorization'] = 'jvhjf';
 axios.defaults.headers.get['Accepts'] = 'applications/json';
 
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request Interceptor', config);
+  return config;
+});
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log('Request Interceptor', res);
+  return res;
+});
+
+axios.interceptors.request.eject(reqInterceptor);
+axios.interceptors.response.eject(resInterceptor);
+
 new Vue({
   el: '#app',
   router,
